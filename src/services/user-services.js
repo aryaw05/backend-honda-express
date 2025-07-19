d3dbf62d00923ca27c38a37ecd52a1d77c7795c5
aryaw05 <aryawahyuaw77@gmail.com>
2025-07-19 20:19:05 +0700

    feat: implement user registration service with validation and error handling

---
+++ src/services/user-services.js
@@ -0,0 +1,30 @@
+import { registerUserValidation } from "../validation/userSchema";
+import { validate } from "../validation/validate";
+import { ResponseError } from "../error/response-error";
+
+import bcrypt from "bcrypt";
+import { prisma } from "../application/database";
+
+const register = async (request) => {
+  const user = validate(registerUserValidation, request);
+
+  const countUser = await prisma.user.count({
+    where: {
+      username: user.username,
+    },
+  });
+
+  if (countUser > 0) {
+    throw new ResponseError(404, "Username already exist");
+  }
+  user.password = await bcrypt.hash(user.password, 10);
+
+  return prisma.user.create({
+    username: user.username,
+    password: user.password,
+    email: user.email,
+    name: user.name,
+  });
+};
+
+export { register };
