953986458dd9a36790c962846eb468dc06fa0c38
aryaw05 <aryawahyuaw77@gmail.com>
2025-07-22 10:17:45 +0700

    bug : fix bug id_user undefined

--- src/controller/motor-controller.js
+++ src/controller/motor-controller.js
@@ -2,8 +2,9 @@ import motorServices from "../services/motor-services.js";
 
 const addMotor = async (req, res, next) => {
   try {
+    const user = req.user;
     const request = req.body;
-    const result = await motorServices.addMotor(request);
+    const result = await motorServices.addMotor(user, request);
     res.status(200).json({ data: result });
   } catch (error) {
     next(error);
--- src/services/motor-services.js
+++ src/services/motor-services.js
@@ -2,8 +2,10 @@ import { addMotorValidation } from "../validation/motorSchema.js";
 import { prisma } from "../application/database.js";
 import { validate } from "../validation/validate.js";
 
-const addMotor = async (request) => {
+const addMotor = async (user, request) => {
   const motors = validate(addMotorValidation, request);
+
+  motors.id_user = user.id;
 
   return prisma.motor.create({
     data: motors,
@@ -17,4 +19,23 @@ const addMotor = async (request) => {
   });
 };
 
-export default { addMotor };
+const getMotor = async () => {
+  return prisma.motor.findMany(
+    {
+      select: {
+        id_motor: true,
+        id_kategori: true,
+        nama_barang: true,
+        deskripsi: true,
+        harga: true,
+        gambar_card: true,
+      },
+    },
+    {
+      orderBy: {
+        id_motor: "asc",
+      },
+    }
+  );
+};
+export default { addMotor, getMotor };
