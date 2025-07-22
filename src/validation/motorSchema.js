202a3d83d5741a8a37185b88a2c902957fabd849
aryaw05 <aryawahyuaw77@gmail.com>
2025-07-22 10:16:49 +0700

    feat  : change motor schema

--- src/validation/motorSchema.js
+++ src/validation/motorSchema.js
@@ -1,6 +1,7 @@
 import Joi from "joi";
 
 const addMotorValidation = Joi.object({
+  id_user: Joi.number().required(),
   id_kategori: Joi.number().required(),
   nama_barang: Joi.string().max(191).required(),
   deskripsi: Joi.string().max(191).required(),
