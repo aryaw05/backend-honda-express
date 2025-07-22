11866cc29026e9a46e87e9ecb46c23921b66cc44
aryaw05 <aryawahyuaw77@gmail.com>
2025-07-22 10:15:54 +0700

    bug : add await to findFirst

--- src/middleware/auth-middleware.js
+++ src/middleware/auth-middleware.js
@@ -1,17 +1,28 @@
 import { prisma } from "../application/database.js";
 
-export const authMiddleware = (req, res, next) => {
+export const authMiddleware = async (req, res, next) => {
   const token = req.get("Authorization");
   if (!token) {
-    res.status(401).json({ message: "Unauthorized" }).end();
+    res
+      .status(401)
+      .json({
+        errors: "Unauthorized",
+      })
+      .end();
   } else {
-    const user = prisma.user.findFirst({
+    // harus menggunakan await karena findFirst mengembalikan promise
+    const user = await prisma.user.findFirst({
       where: {
-        token,
+        token: token,
       },
     });
     if (!user) {
-      res.status(401).json({ message: "Unauthorized" }).end();
+      res
+        .status(401)
+        .json({
+          errors: "Unauthorized",
+        })
+        .end();
     } else {
       req.user = user;
       next();
