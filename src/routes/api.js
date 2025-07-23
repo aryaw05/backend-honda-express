ea1e462691cde20b1c24bd0bba1260410633da8f
aryaw05 <aryawahyuaw77@gmail.com>
2025-07-23 20:25:16 +0700

    add user + motor api

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,5 +1,6 @@
 import kategoriController from "../controller/kategori-controller.js";
 import motorController from "../controller/motor-controller.js";
+import userController from "../controller/user-controller.js";
 import { authMiddleware } from "../middleware/auth-middleware.js";
 
 import express from "express";
@@ -10,8 +11,12 @@ privateRouter.use(authMiddleware);
 
 // Motor Routes
 privateRouter.post("/api/motor", motorController.addMotor);
-
+privateRouter.get("/api/motor/:motorId", motorController.getDetailMotor);
 // Kategori Routes
 privateRouter.post("/api/kategori", kategoriController.addKategori);
+
+privateRouter.delete("/api/users/logout", userController.logout);
+privateRouter.get("/api/users/current", userController.getUser);
+privateRouter.patch("/api/users/update", userController.update);
 
 export { privateRouter };
