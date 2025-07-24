39a5986ca112850e9b96553cf8fbd6b2fef2191f
aryaw05 <aryawahyuaw77@gmail.com>
2025-07-24 20:26:55 +0700

    feat : change userSchema email

--- src/validation/userSchema.js
+++ src/validation/userSchema.js
@@ -2,7 +2,7 @@
 import Joi from "joi";
 const registerUserValidation = Joi.object({
   username: Joi.string().max(100).required(),
-  email: Joi.string().max(100).required(),
+  email: Joi.string().email().max(100).required(),
   password: Joi.string().max(100).required(),
   name: Joi.string().max(100).required(),
 });
