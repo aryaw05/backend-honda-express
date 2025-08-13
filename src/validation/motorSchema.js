6d2e6738e0585ebcd5a7377298a7d2f2c6c594de
aryaw05 <aryawahyuaw77@gmail.com>
2025-08-13 20:23:32 +0700

    feat: remove id_user from addMotorValidation schema

--- src/validation/motorSchema.js
+++ src/validation/motorSchema.js
@@ -1,7 +1,6 @@
 import Joi from "joi";
 
 const addMotorValidation = Joi.object({
-  id_user: Joi.number().required(),
   id_kategori: Joi.number().positive().required(),
   nama_barang: Joi.string().max(191).required(),
   deskripsi: Joi.string().max(191).required(),
