9d83fb7b1aaed7d740ca9908f8d25e660418b2d3
aryaw05 <aryawahyuaw77@gmail.com>
2025-11-08 17:24:40 +0700

    feat(server): update development script and set up express server with middleware

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import { web } from "./application/web.js";
+import express from "express";
 import { logger } from "./application/logger.js";
+import { publicRouter } from "./routes/public-api.js";
+import { privateRouter } from "./routes/api.js";
+import cors from "cors";
+import { errorMiddleware } from "./middleware/error-middleware.js";
 
+const web = express();
 web.listen(3000, () => {
   logger.info("App start");
 });
+web.use(cors());
+
+web.use(express.json());
+web.use(express.static("public"));
+web.use(publicRouter);
+web.use(privateRouter);
+web.use(errorMiddleware);
