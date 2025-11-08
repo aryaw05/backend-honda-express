5ee73ef5feb2d0ba9596206d96c1510b846e1f03
aryaw05 <aryawahyuaw77@gmail.com>
2025-11-08 17:45:05 +0700

    refactor: remove Jest configuration and unused test files
    - Deleted Jest configuration from package.json to simplify setup.
    - Removed test utility for user deletion as it is no longer needed.
    - Eliminated user test file that contained registration tests, streamlining the test suite.

--- test/test-util.js
+++
@@ -1,9 +0,0 @@
-import { prisma } from "../src/application/database.js";
-import bcrypt from "bcrypt";
-export const removeTestUser = async () => {
-  await prisma.user.deleteMany({
-    where: {
-      username: "test",
-    },
-  });
-};
